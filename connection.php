8b7dd678b75be1d0fe1a4af811c33639aeb21153
Hristo <hristo.official@gmial.com>
2017-03-29 22:00:47 +0100

    startingTheProject

---
+++ connection.php
@@ -0,0 +1,9 @@
+<<?php
+
+  $hostname1 = 'localhost';
+  $username1 = 'hskraste';
+  $password1 = '134679';
+  $dbname1 = 'hskraste';
+  $connect = mysqli_connect($hostname, $username, $password, $dbname) or die (mysqli_error($connect));
+
+ ?>
